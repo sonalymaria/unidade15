f1dd8253517da45d6aad2167b9eff0080b57b234
sonalymaria <sonaly07062020@gmail.com>
2026-07-24 12:56:49 -0300

    Add files via upload

---
+++ uc 15/script.js
@@ -0,0 +1,22 @@
+
+
+console.log ("Olá, mundo!")
+console.log ("OI")
+alert ("Boas-vindas")
+confirm ("Você aceita continuar?")
+
+
+let nome = "Soso"
+
+console.log (nome)
+
+let idade = "16"
+
+console.log (idade + " anos")
+
+let cidade = "Natal"
+
+console.log (cidade)
+
+
+
