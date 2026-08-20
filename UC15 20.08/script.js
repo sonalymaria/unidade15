a298d74bd156a2ca828135280017ad0673664d08
sonalymaria <sonaly07062020@gmail.com>
2026-08-20 10:15:15 -0300

    Add files via upload

---
+++ UC15 20.08/script.js
@@ -0,0 +1,66 @@
+// Com if/else - 4 linhas
+
+let mensagens;
+if (nota >=6) {
+    mensagem = 'aprovado';
+} else {
+    mensagem = 'reprovado';
+}
+
+// como terminaria a 1 linha
+
+const mensagem = nota >= 6 ? 'Aprovado' : 'Reprovado';
+
+// Exemplo 1
+
+let descricao;
+
+if (temperatura > 30) {
+
+  descricao = 'Quente';
+
+} else {
+
+  descricao = 'Agradável';
+
+}
+
+// resposta
+
+const descrição = temperatura > 30 'Quente' : 'Agradável';
+
+// Exemplo 2
+
+let tipo;
+
+if (numero % 2 === 0) {
+
+  tipo = 'par';
+
+} else {
+
+  tipo = 'ímpar';
+
+}
+
+// resposta
+
+const tipo = numero % 2 'Par' : 'Ímpar';
+
+// Exemplo 3
+
+let saudacao;
+
+if (hora < 12) {
+
+  saudacao = 'Bom dia';
+
+} else {
+
+  saudacao = 'Boa tarde/noite';
+
+}
+
+// resposta
+
+const saudação = hora < 2 'Bom dia' : 'Boa tarde/noite';
